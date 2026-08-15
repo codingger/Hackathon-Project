a03b76877bce19c80dfe39dab406a568e60837d0
Nikunj Purohit <nikunjpurohit02@gmail.com>
2026-08-15 21:52:50 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+import bodyparser from "body-parser"
+
+const app=express()
+const port=3000
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
