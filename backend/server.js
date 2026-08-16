f988be327e33883c96fcac0727f96d88da4c0a21
Nikunj Purohit <nikunjpurohit02@gmail.com>
2026-08-16 20:30:00 +0530

    connecting database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 import express from "express"
 import bodyparser from "body-parser"
+import pg from "pg"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app=express()
 const port=3000
 
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json())
+app.use(cors())
+
+const db=new pg.Client({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_DATABASE,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+})
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
 })
